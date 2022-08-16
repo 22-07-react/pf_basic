4acf6a327f23ae7e01415913000479191fbd3709
tubi55 <tubi55@nate.com>
2022-08-16 15:42:13 +0900

    버튼 클릭시 스크롤 이동

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,18 +5,18 @@ import Pics from './Pics';
 import Vids from './Vids';
 import Btns from './Btns';
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import Anime from '../../assets/Anime';
 
 function Main() {
-	console.log('main called');
 	const main = useRef(null);
 	const pos = useRef([]);
+	const [Index, setIndex] = useState(0);
 
 	const getPos = () => {
 		pos.current = [];
 		const secs = main.current.querySelectorAll('.myScroll');
 		for (const sec of secs) pos.current.push(sec.offsetTop);
-		console.log(pos.current);
 	};
 
 	useEffect(() => {
@@ -28,6 +28,14 @@ function Main() {
 		};
 	}, []);
 
+	useEffect(() => {
+		new Anime(window, {
+			prop: 'scroll',
+			value: pos.current[Index],
+			duration: 500,
+		});
+	}, [Index]);
+
 	return (
 		<main ref={main}>
 			<Header type={'main'} />
@@ -35,7 +43,7 @@ function Main() {
 			<News />
 			<Pics />
 			<Vids />
-			<Btns />
+			<Btns setIndex={setIndex} />
 		</main>
 	);
 }
