59212380037231b4713615b8cfeaa79862722a74
tubi55 <tubi55@nate.com>
2022-08-16 10:03:31 +0900

    수정모드 스타일링

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -27,6 +27,16 @@ function Community() {
 		setPosts(newPosts);
 	};
 
+	//글 수정모드 변경함수
+	const enableUpdate = (index) => {
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
 	useEffect(() => {
 		console.log(Posts);
 	}, [Posts]);
@@ -48,15 +58,39 @@ function Community() {
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
-							<div className='txt'>
-								<h2>{post.title}</h2>
-								<p>{post.content}</p>
-							</div>
+							{post.enableUpdate ? (
+								//수정모드
+								<>
+									<div className='editTxt'>
+										<input type='text' defaultValue={post.title} />
+										<br />
+										<textarea
+											name=''
+											id=''
+											cols='30'
+											rows='3'
+											defaultValue={post.content}></textarea>
+										<br />
+									</div>
+									<div className='btnSet'>
+										<button>CANCEL</button>
+										<button>UPDATE</button>
+									</div>
+								</>
+							) : (
+								//출력모드
+								<>
+									<div className='txt'>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+									</div>
 
-							<div className='btnSet'>
-								<button>EDIT</button>
-								<button onClick={() => deletePost(idx)}>DELETE</button>
-							</div>
+									<div className='btnSet'>
+										<button onClick={() => enableUpdate(idx)}>EDIT</button>
+										<button onClick={() => deletePost(idx)}>DELETE</button>
+									</div>
+								</>
+							)}
 						</article>
 					);
 				})}
